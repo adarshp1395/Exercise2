09a93809c92b6d4db613df246dbcf00857960881
Adarsh Patel <adarshp1395@gmail.com>
2020-01-21 21:16:27 -0500

    Commit Ex2

---
+++ Exercise2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Excercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age;
+            int height;
+            int heartrate;
+            // Max heart rate = 220 - age
+
+            age = 24;
+            height = 161;
+            heartrate = 220 - age;
+            Console.WriteLine("I am " + age + "years old. Max heartrate is " + heartrate);
+        }
+    }
+}
